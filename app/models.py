e5e8d4f4a86fcd197e3072600c3425044ab79398
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-03-06 16:55:29 +0200

    Add App app (for mobile app)

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+from enumfields import Enum, EnumField
+
+
+class Permission(Enum):
+    ADMIN = 'A'
+    MANAGER = 'M'
+    SUPPLIER = 'S'
+    REGULAR = 'R'
+
+
+class AppUser(models.Model):
+    class Meta:
+        db_table = '"app_users"'
+    member = models.ForeignKey("members.Member", on_delete=models.CASCADE)
+    permission = EnumField(Permission, max_length=1, name=_('permission'), default=Permission.REGULAR)
+    password_hash = models.CharField(_('password_hash'), max_length=128, blank=True)
--- bigsister/settings.py
+++ bigsister/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'members.apps.MembersConfig',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
